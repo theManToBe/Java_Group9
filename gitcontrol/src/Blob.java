d61cfefe92b17767413d9ba2ac47dd533faf4246
withjiang <70436180+withjiang@users.noreply.github.com>
2020-12-31 00:03:54 +0800

    Add files via upload

---
+++ gitcontrol/src/Blob.java
@@ -0,0 +1,24 @@
+pimport java.io.*;
+
+public class Blob extends KeyValueObject {
+    public Blob(File file) throws Exception {
+        generateKey(file);//构造时生成file的key
+        this.type = "blob";//表明文件类型
+
+
+    }
+
+
+    public String getType(){
+        return type;
+    }
+
+    public String getKey(){
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return "100644 blob " + key;
+    }
+}
